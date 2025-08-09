5984f1c91cd3b6b6d3573c5f74bc03a6955f6ecf
Raushan Kumar <raushankumar93341@gmail.com>
2025-08-09 10:07:30 +0530

    adding char

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,51 +1,86 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
+using namespace std;
 
-bool match_pattern(const std::string &input_line, const std::string &pattern) {
-    if (pattern.length() == 1 && std::isalpha(pattern[0])) {
-        return input_line.find(pattern) != std::string::npos;
-    } else if (pattern == "\\d") {
-        return input_line.find_first_of("123456789") != std::string::npos;
-    } else if (pattern == "\\w") {
-        return input_line.find_first_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_") != std::string::npos;
-    } else {
-        throw std::runtime_error("Unhandled pattern " + pattern);
+bool match_pattern(const string& input_line, const string& pattern) {
+    if (pattern.length() == 1) {
+        return input_line.find(pattern) != string::npos;
+    }
+    else if (pattern == "\\d") { // match digits
+        return input_line.find_first_of("0123456789") != string::npos;
+    }else if(pattern == "\\w"){
+        return input_line.find_first_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") != string::npos;
+    }
+    else if (pattern == "\\s") { // match whitespace
+        for (const auto& l : input_line) {
+            if (isspace(static_cast<unsigned char>(l))) {
+                return true;
+            }
+        }
+        return false;
+    }
+    else if (pattern == "\\w") { // match alphanumeric
+        for (const auto& l : input_line) {
+            if (isdigit(static_cast<unsigned char>(l)) || isalpha(static_cast<unsigned char>(l))) {
+                return true;
+            }
+        }
+        return false;
+    }
+    else if (*pattern.begin() == '[' && *(pattern.end() - 1) == ']') {
+        const string group = pattern.substr(1, pattern.length() - 2);
+        const bool isNegativeGroup = group[0] == '^';
+
+        if (isNegativeGroup) {
+            for (const auto& l : group.substr(1)) {
+                if (input_line.find(l) != string::npos) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        for (const auto& l : group) {
+            if (input_line.find(l) != string::npos) {
+                return true;
+            }
+        }
+        return false;
+    }
+    else {
+        // Default: substring match for any other pattern
+        return input_line.find(pattern) != string::npos;
     }
 }
-int main(int argc, char* argv[]) {
-    // Flush after every std::cout / std::cerr
-    std::cout << std::unitbuf;
-    std::cerr << std::unitbuf;
 
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cerr << "Logs from your program will appear here" << std::endl;
+int main(int argc, char* argv[]) {
+    cout << unitbuf;
+    cerr << unitbuf;
 
     if (argc != 3) {
-        std::cerr << "Expected two arguments" << std::endl;
+        cerr << "Expected two arguments" << endl;
         return 1;
     }
 
-    std::string flag = argv[1];
-    std::string pattern = argv[2];
+    string flag = argv[1];
+    string pattern = argv[2];
 
     if (flag != "-E") {
-        std::cerr << "Expected first argument to be '-E'" << std::endl;
+        cerr << "Expected first argument to be '-E'" << endl;
         return 1;
     }
 
-    // Uncomment this block to pass the first stage
-    //
-    std::string input_line;
-    std::getline(std::cin, input_line);
-    
+    string input_line;
+    getline(cin, input_line);
+
     try {
         if (match_pattern(input_line, pattern)) {
-            return 0;
+            return 0; // match found
         } else {
-            return 1;
+            return 1; // no match
         }
-    } catch (const std::runtime_error& e) {
-        std::cerr << e.what() << std::endl;
+    }
+    catch (const runtime_error& e) {
+        cerr << e.what() << endl;
         return 1;
     }
 }
