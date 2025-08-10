e08484d99577d160f88738d8ad17973bcdee2370
Raushan Kumar <raushankumar93341@gmail.com>
2025-08-10 12:21:38 +0530

    commiting

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,57 +1,58 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
-bool match_pattern(const string &input_line, const string &pattern) {
-    if (pattern.length() == 1 && isalpha(pattern[0])) {
-        return input_line.find(pattern) != string::npos;
-    } 
-    else if (pattern == "\\d") {
-        return input_line.find_first_of("0123456789") != string::npos;
-    } 
-    else if (pattern == "\\w") {
-        return input_line.find_first_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_") != string::npos;
+int tokenize_step(int i, const string& pattern) {
+    if (pattern[i] == '[') {
+        while (++i < pattern.size() && pattern[i] != ']');
+    } else if (pattern[i] == '\\') {
+        ++i; // skip the escape
     }
-    else if (pattern.length() >= 2 && pattern[0] == '[' && pattern[pattern.length() - 1] == ']') {
-        if (pattern[1] == '^')
-            return input_line.find_first_not_of(pattern.substr(2, pattern.size() - 3)) != string::npos;
-        return input_line.find_first_of(pattern.substr(1, pattern.size() - 2)) != string::npos;
-    } 
-    else { // Multi-token sequential pattern
-        auto matches_token = [&](char pat, char ch) {
-            if (pat == 'd') return isdigit(static_cast<unsigned char>(ch));
-            if (pat == 'w') return isalnum(static_cast<unsigned char>(ch)) || ch == '_';
-            return false;
-        };
+    return i + 1;
+}
 
-        for (size_t start = 0; start < input_line.size(); ++start) {
-            size_t i = start;
-            size_t j = 0;
-            bool matched = true;
+void tokenize_pattern(const string& pattern, vector<string>& tokens) {
+    int i = 0;
+    while (i < pattern.size()) {
+        int j = tokenize_step(i, pattern);
+        tokens.push_back(pattern.substr(i, j - i));
+        i = j;
+    }
+}
 
-            while (j < pattern.size() && i < input_line.size()) {
-                if (pattern[j] == '\\') {
-                    if (j + 1 >= pattern.size() || !matches_token(pattern[j + 1], input_line[i])) {
-                        matched = false; break;
-                    }
-                    j += 2; i += 1;
-                }
-                else if (pattern[j] == '[') {
-                    bool negate = (j + 1 < pattern.size() && pattern[j + 1] == '^');
-                    size_t end = pattern.find(']', j + 1);
-                    if (end == string::npos) return false; // invalid
-                    string chars = pattern.substr(j + (negate ? 2 : 1), end - j - (negate ? 2 : 1));
-                    bool in_set = chars.find(input_line[i]) != string::npos;
-                    if ((negate && in_set) || (!negate && !in_set)) { matched = false; break; }
-                    j = end + 1; i += 1;
-                }
-                else {
-                    if (pattern[j] != input_line[i]) { matched = false; break; }
-                    j += 1; i += 1;
-                }
-            }
+bool match_token(const string& token, char c) {
+    if (token.size() == 1) {
+        return c == token[0];
+    } else if (token == "\\d") {
+        return isdigit(c);
+    } else if (token == "\\w") {
+        return c == '_' || isalnum(c);
+    } else if (token.size() > 2 && token[0] == '[' && token.back() == ']') {
+        if (token[1] == '^')
+            return token.substr(2, token.size() - 3).find(c) == string::npos;
+        return token.substr(1, token.size() - 2).find(c) != string::npos;
+    }
+    throw runtime_error("Unhandled token " + token);
+}
 
-            if (matched && j == pattern.size()) return true;
+bool match_sequence_at(int pos, const string& input, const vector<string>& tokens) {
+    if (pos + tokens.size() > input.size()) return false;
+    for (size_t i = 0; i < tokens.size(); i++) {
+        if (!match_token(tokens[i], input[pos + i])) return false;
+    }
+    return true;
+}
+
+bool match_pattern(const string &input_line, const string &pattern) {
+    vector<string> tokens;
+    tokenize_pattern(pattern, tokens);
+
+    if (tokens.size() == 1) {
+        return match_token(tokens[0], input_line[input_line.find_first_not_of("")]);
+    } else {
+        for (int i = 0; i <= (int)input_line.size() - (int)tokens.size(); i++) {
+            if (match_sequence_at(i, input_line, tokens)) return true;
         }
         return false;
     }
@@ -60,8 +61,6 @@ bool match_pattern(const string &input_line, const string &pattern) {
 int main(int argc, char* argv[]) {
     cout << unitbuf;
     cerr << unitbuf;
-
-    cerr << "Logs from your program will appear here" << endl;
 
     if (argc != 3) {
         cerr << "Expected two arguments" << endl;
@@ -78,7 +77,7 @@ int main(int argc, char* argv[]) {
 
     string input_line;
     getline(cin, input_line);
-    
+
     try {
         if (match_pattern(input_line, pattern)) {
             return 0;
